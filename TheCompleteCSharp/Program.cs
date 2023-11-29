fc5eccf7d1df069261086bf265d9222a1b87e731
ArmanKhalili82 <hykhalili@yahoo.com>
2023-11-29 10:14:53 +0100

    Add project files.

---
+++ TheCompleteCSharp/Program.cs
@@ -0,0 +1,4 @@
+﻿using TheCompleteCSharp;
+
+var cool = new ShowMaxChar();
+cool.Show();
---
+++ TheCompleteCSharp/ShowMaxChar.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TheCompleteCSharp
+{
+    public class ShowMaxChar
+    {
+        public void Show()
+        {
+            Console.WriteLine("Enter Your Name:");
+            var str = Console.ReadLine();
+            var res = str.GroupBy(p => p).Select(p => new { Count = p.Count(), Char = p.Key }).GroupBy(p => p.Count, p => p.Char).OrderByDescending(p => p.Key).First();
+
+            foreach (var r in res)
+            {
+                Console.WriteLine("{0}: {1}", res.Key, r);
+            }
+        }
+    }
+}
